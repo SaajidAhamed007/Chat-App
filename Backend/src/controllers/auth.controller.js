2e0607be0c838263627c0e5b6838584dcdb1b181
SaajidAhamed007 <saajidahamed01@gmail.com>
2025-07-13 08:37:46 +0530

    Update auth.controller.js

--- Backend/src/controllers/auth.controller.js
+++ Backend/src/controllers/auth.controller.js
@@ -68,7 +68,7 @@ export const login = async (req,res) => {
             return res.status(400).json({message:"Invalid credentials"})
         }
 
-        const token = generateToken(user._id);
+        const token = generateToken(user._id,res);
 
         return res.status(200).json({ 
             _id:user._id,
